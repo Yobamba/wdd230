71d134ecbdb82d770e05804de60d6e1230ae096f
Ednilson <ednilsoncastanheira53@gmail.com>
2023-01-23 21:35:45 +0000

    fixed a button on the discover paged

--- chamber/scripts/responsive.js
+++ chamber/scripts/responsive.js
@@ -1,10 +1,14 @@
-const menubutton = document.querySelector(".menu-button");
-const menuItems = document.querySelector(".menu-item")
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
             
 menubutton.addEventListener("click", () => {
     menuitems.forEach((item) => item.classList.toggle("open"));
     menubutton.classList.toggle("close");
-    });
+});
 
 
+
+
+    
+ 
     
