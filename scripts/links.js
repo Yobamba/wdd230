b1a9214e257803a16feb4178c33e6f6f1726c3d5
Ednilson <ednilsoncastanheira53@gmail.com>
2023-02-05 02:29:15 +0000

    updated the json

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,7 @@
 const baseUrl = "https://yobamba.github.io/wdd230/";
-const weeks = document.querySelectorAll("#weeks");
+var weekUList = document.querySelector("#weeks");
+const linkList = document.querySelectorAll('.link-list');
+
 
 const localUrl = "links.json";
 
@@ -9,12 +11,28 @@ async function apiFetch2() {
         let response = await fetch(localUrl);
         if (response.ok) {
             let data = await response.json();
+            let i = 1;
             
-            weeklyLinks = data.week01;
+            
+            weeklyLinks = data.weeks;
+
             console.log(weeklyLinks);
+            
+            
+            // console.log(weeklyLinks);
+            // populateLinks(weeklyLinks);
+            // // console.log(weeklyLinks.keys());
+            // console.log(weeklyLinks);
             weeklyLinks.forEach(link => {
-                console.log(link);
+                populateLinks(link.week, link.links, link.name );
+                // console.log(link.week);
+                // console.log(link.links);
+                // console.log(link.name);
+                 
             });
+           
+            // setHref(weeklyLinks);
+
             // displayResults2(data)
             // Then we'll want to loop through each attribute
             // data[0].forEach(week => {
@@ -31,7 +49,142 @@ async function apiFetch2() {
 
 apiFetch2();
 
-function populateLinks(link) {
 
+
+function populateLinks(weeks, links, names) {
+
+    let newli = document.createElement('li');
+   
+        // create an a element
+    let newA = document.createElement('a');
+        // set the attributes
+    links.forEach(link => {
+        newA.setAttribute('href', link);
+        newA.setAttribute('target', '_blank');
+
+            // Set the link text
+        let nodeText = document.createTextNode(names);
+        
+
+        newA.setAttribute('title', 'link');
+
+            // put the text inside the link
+        newA.appendChild(nodeText);
+        console.log(newA);
+            // put the link inside the 
+            // newP.appendChild(newA);
+        
+
+            //test this 
+        newli.innerHTML = `${weeks}: <a href="${newA}" target="_blank">${names}</a>|`;
+        console.log(newli);
+        weekUList.appendChild(newli);
+    });
+    
+
+        
+
+    
+    
+    
+
+
+
+
+
+
+    // for (let i = 0; i < link.length;i++) {
+        
+    //     let newli = document.createElement('li');
+    //     let newP = document.createElement('p');
+    //     // create an a element
+    //     let newA = document.createElement('a');
+    //     // set the attributes
+    //     newA.setAttribute('href', link)
+    //     newA.setAttribute('target', '_blank');
+
+    //     // Set the link text
+    //     let nodeText = document.createTextNode(link);
+
+    //     newA.setAttribute('title', 'link');
+
+    //     // put the text inside the link
+    //     newA.appendChild(nodeText);
+    //     // put the link inside the 
+    //     // newP.appendChild(newA);
+    //     newP.textContent = `Week ${i + 1}`;
+
+    //     //test this 
+    //     weeks.appendChild(newA);
+
+        
+
+    
+    //     newli.appendChild(newP);
+    //     weeks.appendChild(newli);
+    
+        
+        
+
+        // let text = `Week${i + 1} `
+
+        // 1. Leave the empty link on the home page
+        // 2. Add the same class to all of those links
+        // 3. Use querySelectorAll to target all of those links of that same class
+        // 4. Set the attributes of each link
+    }
+
+
+// }
+
+function setHref(links) {
+    links.forEach(link => {
+        console.log(`First for each ${link}`)
+        linkList.forEach(arrayLink => {
+            console.log(`Second for each ${arrayLink}`)
+            arrayLink.setAttribute('href', link);
+        })
+    });
+    // for (let i = 0; i < link.length; i++) {
+
+        
+    //     // now we have a loop that makes our index start at 0
+    //     // what I need now is to make it get the first item in the JSON array o links
+    //     linkList[i].setAttribute('href', link);
+    //     // let firstLink = link;
+    //     console.log(`${linkList[i]} ${i}`); 
+        
+    // }
 }
 
+function populateActivities(link) {
+    let newli = document.createElement('li');
+    let newP = document.createElement('p');
+    // create an a element
+    let newA = document.createElement('a');
+    // set the attributes
+    newA.setAttribute('href', link.links)
+    newA.setAttribute('target', '_blank');
+
+        // Set the link text
+    let nodeText = document.createTextNode(link.name);
+
+    newA.setAttribute('title', 'link');
+
+        // put the text inside the link
+    newA.appendChild(nodeText);
+        // put the link inside the 
+        // newP.appendChild(newA);
+    // newP.textContent = `Week${1}`;
+    // console.log(newA);
+    newli.innerHTML = `<p>${link.week}<a href="${link.links}"></a></p>`;
+
+        //test this 
+    weeks.appendChild(newA);
+
+        
+
+    
+    newli.appendChild(newP);
+    weeks.appendChild(newli);
+}
